d6fec90de1caa027ee3041cb2cc4bf75d6cb6e3d
padrinoo2 <padrinoo200@gmail.com>
2016-03-02 11:53:32 +0330

    tg

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {156218623},
     admin_users = {},
     disabled_channels = {}
   }
